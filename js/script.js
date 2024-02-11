06332e60eb48f956c9b04c9eb5f9447b3ad4dd47
Luis Antonio <luissantosfilho2014@gmail.com>
2024-02-11 15:00:08 -0300

    Icon added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ const headerEl = document.querySelector(".main-header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// Function to close the mobile navigation
+function closeMobileNavigation() {
+  headerEl.classList.remove("nav-open");
+}
+
+// Select all links in the mobile navigation
+const mobileNavLinks = document.querySelectorAll(".main-nav-link");
+
+// Add click event to each link
+mobileNavLinks.forEach(function (link) {
+  link.addEventListener("click", closeMobileNavigation);
+});
