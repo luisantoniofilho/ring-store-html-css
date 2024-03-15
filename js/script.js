857d43fd2fca3844f6f43a992bc59e2a8e824a7e
Luis Antonio <luissantosfilho2014@gmail.com>
2024-03-14 22:49:39 -0300

    Created smooth navigation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 // Make mobile navigation work
-
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".main-header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
-
-// Function to close the mobile navigation
-function closeMobileNavigation() {
-  headerEl.classList.remove("nav-open");
-}
-
-// Select all links in the mobile navigation
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
 const mobileNavLinks = document.querySelectorAll(".main-nav-link");
 
-// Add click event to each link
+function toggleMenu() {
+  document.querySelector(".main-header").classList.toggle("nav-open");
+}
+
+// Open menu
+openMenuBtn.addEventListener("click", toggleMenu);
+
+// Close menu
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+// CLose menu when mobile nav clicked
 mobileNavLinks.forEach(function (link) {
-  link.addEventListener("click", closeMobileNavigation);
+  link.addEventListener("click", toggleMenu);
 });
+
+// Smooth navigation
+const meetCollectionBtn = document.querySelector(".meet-collection-btn");
+const buyBtn = document.querySelectorAll(".best-btn-buy");
+const ctaSection = document.querySelector(".section-cta");
+
+const goToCtaSection = () => ctaSection.scrollIntoView({ behavior: "smooth" });
+
+meetCollectionBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  goToCtaSection();
+});
+
+buyBtn.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    goToCtaSection();
+  })
+);
