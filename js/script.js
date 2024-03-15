4db31404975b527edf3247957cbf9a04d0bda828
Luis Antonio <luissantosfilho2014@gmail.com>
2024-03-15 20:31:21 -0300

    Smooth navigation on main links

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-// Make mobile navigation work
+// Selecting elements
 const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
 const mobileNavLinks = document.querySelectorAll(".main-nav-link");
+const meetCollectionBtn = document.querySelector(".meet-collection-btn");
+const buyBtn = document.querySelectorAll(".best-btn-buy");
+const ctaSection = document.querySelector(".section-cta");
 
+////////////////////////////////////////////
+// Make mobile navigation work
 function toggleMenu() {
   document.querySelector(".main-header").classList.toggle("nav-open");
 }
@@ -18,10 +23,18 @@ mobileNavLinks.forEach(function (link) {
   link.addEventListener("click", toggleMenu);
 });
 
+////////////////////////////////////////////
 // Smooth navigation
-const meetCollectionBtn = document.querySelector(".meet-collection-btn");
-const buyBtn = document.querySelectorAll(".best-btn-buy");
-const ctaSection = document.querySelector(".section-cta");
+const navLinks = document.querySelector(".main-nav-list");
+
+navLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("main-nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
 
 const goToCtaSection = () => ctaSection.scrollIntoView({ behavior: "smooth" });
 
