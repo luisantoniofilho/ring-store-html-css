ed978f070b38ba816c1fa2afea07ea6d9cc549d0
Luis Antonio <luissantosfilho2014@gmail.com>
2024-04-20 10:18:24 -0300

    Bugs on modal window fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,43 @@
-"use strict";
+'use strict';
 
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 // Selecting elements
-const navLinks = document.querySelector(".main-nav-list");
-const mobileNavLinks = document.querySelectorAll(".main-nav-link");
-const openMenuBtn = document.querySelector(".open-menu-btn");
-const closeMenuBtn = document.querySelector(".close-menu-btn");
-const meetCollectionBtn = document.querySelector(".meet-collection-btn");
-const buyBtn = document.querySelectorAll(".best-btn-buy");
-const ctaSection = document.querySelector(".section-cta");
-const ctaForm = document.querySelector(".cta-form");
+const navLinks = document.querySelector('.main-nav-list');
+const mobileNavLinks = document.querySelectorAll('.main-nav-link');
+const openMenuBtn = document.querySelector('.open-menu-btn');
+const closeMenuBtn = document.querySelector('.close-menu-btn');
+const meetCollectionBtn = document.querySelector('.meet-collection-btn');
+const buyBtn = document.querySelectorAll('.best-btn-buy');
+const ctaSection = document.querySelector('.section-cta');
+const ctaForm = document.querySelector('.cta-form');
+const modalWindow = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.btn-close-modal');
+const overlay = document.querySelector('.overlay');
 
 ////////////////////////////////////////////
 // Smooth navigation
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener('click', e => {
   e.preventDefault();
 
-  if (e.target.classList.contains("main-nav-link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  if (e.target.classList.contains('main-nav-link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 const goToCtaSection = function () {
-  ctaSection.scrollIntoView({ behavior: "smooth" });
+  ctaSection.scrollIntoView({ behavior: 'smooth' });
 };
 
-meetCollectionBtn.addEventListener("click", (e) => {
+meetCollectionBtn.addEventListener('click', e => {
   e.preventDefault();
   goToCtaSection();
 });
 
-buyBtn.forEach((btn) =>
-  btn.addEventListener("click", (e) => {
+buyBtn.forEach(btn =>
+  btn.addEventListener('click', e => {
     e.preventDefault();
     goToCtaSection();
   })
@@ -45,23 +48,35 @@ buyBtn.forEach((btn) =>
 
 // Activate mobile menu
 function toggleMenu() {
-  document.querySelector(".main-header").classList.toggle("nav-open");
+  document.querySelector('.main-header').classList.toggle('nav-open');
+  openMenuBtn.classList.toggle('hidden');
+  closeMenuBtn.classList.toggle('hidden');
 }
 
 // Open menu
-openMenuBtn.addEventListener("click", toggleMenu);
+openMenuBtn.addEventListener('click', toggleMenu);
 
 // Close menu
-closeMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
 
 // CLose menu when mobile nav clicked
 mobileNavLinks.forEach(function (link) {
-  link.addEventListener("click", toggleMenu);
+  link.addEventListener('click', toggleMenu);
 });
 
 ////////////////////////////////////////////
 // Modal Window
-ctaForm.addEventListener("submit", function (e) {
+const openCloseModal = function () {
+  modalWindow.classList.toggle('hidden');
+  // closeModalBtn.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+ctaForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  document.querySelector(".modal").classList.toggle("hidden");
+  openCloseModal();
+});
+
+closeModalBtn.addEventListener('click', function () {
+  openCloseModal();
 });
